503f53d880dfd533562963dfab2389f3dcc4d5ea
Paul Gailey <linenoiz@gmail.com>
2021-05-15 08:44:07 -0600

    Added HistoryCreate and HistoryListItem models

--- FLAPI.Models/HistoryListItem.cs
+++ FLAPI.Models/HistoryListItem.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace FLAPI.Models
 {
-    public class HistoryCreate
+    public class HistoryListItem
     {
+        public int Id { get; set; }
         public string EventName { get; set; }
         public DateTime EventDate { get; set; }
         public string Description { get; set; }
