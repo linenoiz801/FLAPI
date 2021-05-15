9a4dc7d0d0e817c837275426c35a213ab7ee025f
Adam Cohill <adamcohill16@gmail.com>
2021-05-15 12:46:47 -0400

    Get and Create service complete

---
+++ FLAPI.Services/LocationService.cs
@@ -0,0 +1,50 @@
+﻿using FLAPI.Data;
+using FLAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLAPI.Services
+{
+    class LocationService
+    {
+
+        public bool CreateLocation(LocationCreate model)
+        {
+            var entity =
+                new Location()
+                {
+                    Country=model.Country,
+                    MetroArea=model.MetroArea,
+                    Name=model.Name
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Locations.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<LocationListItem> GetLocations()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Locations
+                    .Select(
+                        e =>
+                        new LocationListItem
+                        {
+                            Name = e.Name,
+                            Country = e.Country,
+                            MetroArea = e.MetroArea
+                        }
+                                );
+                return query.ToArray();
+            }
+
+        }
+    }
+}
