b55bff136a5ecd93a7e2ce751ed33ae0db2c144c
Paul Gailey <linenoiz@gmail.com>
2021-05-15 09:17:47 -0700

    Added GetHistoryById method to HistoryService

--- FLAPI.Services/HistoryService.cs
+++ FLAPI.Services/HistoryService.cs
@@ -67,6 +67,24 @@ namespace FLAPI.Services
                 return query.ToArray();
             }
         }
+        public HistoryListItem GetHistoryById(int historyId)
+        {
+            HistoryListItem result = new HistoryListItem();
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Histories
+                        .Single((b => b.Id == historyId));
+
+                result.Id = query.Id;
+                result.EventName = query.EventName;
+                result.EventDate = query.EventDate;
+                result.Description = query.Description;
+
+                return result;
+            }
+        }
         public bool UpdateHistory(HistoryListItem model)
         {
             using (var ctx = new ApplicationDbContext())
@@ -76,7 +94,7 @@ namespace FLAPI.Services
                         .Histories
                         .SingleOrDefault(e => e.Id == model.Id);
 
-                if (query !=  null)
+                if (query != null)
                 {
                     query.EventDate = model.EventDate;
                     query.EventName = model.EventName;
