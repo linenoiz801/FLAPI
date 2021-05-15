4427af1102b0e23163614a540785086515812f5a
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-05-15 14:49:06 -0400

    added Character Entity

---
+++ FlAPI.Data/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLAPI.Data
+{
+    public class Character
+    {
+        public int CharacterId { get; set; }
+        public string CharacterName { get; set; }
+        public int Age { get; set; }
+        public string Affiliation { get; set; }
+        public bool IsNPC { get; set; }
+        public bool IsHostile { get; set; }
+    }
+}
