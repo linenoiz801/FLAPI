3442d4e1d04c2428db2cece2668cb57cc6f1f92c
Paul Gailey <linenoiz@gmail.com>
2021-05-15 08:32:29 -0600

    Created History entity

---
+++ FlAPI.Data/History.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLAPI.Data
+{
+    public class History
+    {
+        /*  
+        Id int [pk, increment]
+        EventName string
+        EventDate datetime
+        Description string*/
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string EventName { get; set; }
+        [Required]
+        public DateTime EventDate { get; set; }
+        public string Description { get; set; }
+    }
+}
