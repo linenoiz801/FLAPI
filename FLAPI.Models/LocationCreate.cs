220fbc2ded26231a99fcb07ddd4c47acea552342
Adam Cohill <adamcohill16@gmail.com>
2021-05-15 11:33:03 -0400

    ModelsDone

---
+++ FLAPI.Models/LocationCreate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLAPI.Models
+{
+    public class LocationCreate
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "Please Enter More Charaters.")]
+        [MaxLength(6000, ErrorMessage = "You Entered Too Many Characters.")]
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public string MetroArea { get; set; }
+    }
+}
