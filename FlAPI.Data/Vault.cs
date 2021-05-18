a85223f57e8c88ee2055c648450d5e2770a116d9
JoshuaMDenman <joshuamdenman@gmail.com>
2021-05-18 19:27:00 -0400

    Added vault entity annotations

--- FlAPI.Data/Vault.cs
+++ FlAPI.Data/Vault.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace FLAPI.Data
 {
     public class Vault
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string VaultName { get; set; }
+        [Required]
         public string VaultNumber { get; set; }
     }
 }
