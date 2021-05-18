119153e6f9aae8df4ef242d9228743edaf376aaa
JoshuaMDenman <joshuamdenman@gmail.com>
2021-05-18 19:57:19 -0400

    Added Vault Services

---
+++ FLAPI.Services/VaultServices.cs
@@ -0,0 +1,28 @@
+﻿using FLAPI.Data;
+using FLAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLAPI.Services
+{
+    public class VaultServices
+    {
+        public bool CreateVault(VaultCreate model)
+        {
+            var entity =
+                new Vault()
+                {
+                    VaultName = model.VaultName,
+                    VaultNumber = model.VaultNumber
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Vaults.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
