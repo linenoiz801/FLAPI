330a3f6929d3b8609170efb15a5a5bab2b2c5d48
JoshuaMDenman <joshuamdenman@gmail.com>
2021-05-18 20:06:36 -0400

    Got all object methods

--- FLAPI.Services/VaultServices.cs
+++ FLAPI.Services/VaultServices.cs
@@ -24,5 +24,24 @@ namespace FLAPI.Services
                 return ctx.SaveChanges() == 1;
             }
         }
+        public IEnumerable<VaultListItem> GetVaults()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Vaults
+                    .Select(
+                        e =>
+                        new VaultListItem
+                        {
+                            VaultId=e.Id,
+                            VaultName=e.VaultName,
+                            VaultNumber=e.VaultNumber
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
     }
 }
