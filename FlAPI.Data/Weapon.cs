84a51032faeefdcabc0251621132d7dc2869e56e
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-05-20 19:02:45 -0400

    updated annotations in Weapon class

--- FlAPI.Data/Weapon.cs
+++ FlAPI.Data/Weapon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,13 +28,17 @@ namespace FLAPI.Data
     //}
     public class Weapon
     {
+        [Key]
         public int WeaponId { get; set; }
+        [Required]
         public string WeaponName { get; set; }
+        [Required]
         public string Description { get; set; }
         public string AmmoType { get; set; }
+        [Required]
         public string WeaponType { get; set; }
         public string BaseDamage { get; set; }
-        // public enum AmmoType TypeOfAmmo { get; set; }
-        // public enum WeaponType TypeOfWeapon { get; set; }
+        // *Stretch: public enum AmmoType TypeOfAmmo { get; set; }
+        // *Stretch: public enum WeaponType TypeOfWeapon { get; set; }
     }
 }
