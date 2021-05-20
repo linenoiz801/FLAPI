0042abcc3662a78fd9fb721bbf5f5714368f4908
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-05-20 19:16:39 -0400

    created Characters_Vaults Entity in .Data

---
+++ FlAPI.Data/Characters_Vaults.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLAPI.Data
+{
+    public class Characters_Vaults
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Character))]
+        public int CharacterId { get; set; }
+        public virtual Character Character { get; set; }
+        [ForeignKey(nameof(Vault))]
+        public int VaultId { get; set; }
+        public virtual Vault Vault { get; set; }
+    }
+}
