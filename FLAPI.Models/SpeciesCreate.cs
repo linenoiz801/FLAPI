d438c1119e6affbd1f09e81210c5fe5dc4b8d0c4
JoshuaMDenman <joshuamdenman@gmail.com>
2021-05-20 18:35:10 -0400

    Created Species Models

---
+++ FLAPI.Models/SpeciesCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLAPI.Models
+{
+    class SpeciesCreate
+    {
+        [Required]
+        public string GameName { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
