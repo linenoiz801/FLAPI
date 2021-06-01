1b9a8e192f0a981c293b6b0135572c34000945fc
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-05-31 21:02:57 -0400

    added migration

---
+++ FlAPI.Data/Migrations/202106010102018_Adrianna2.cs
@@ -0,0 +1,18 @@
+﻿namespace FLAPI.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Adrianna2 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Characters", "Affiliation", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Characters", "Affiliation", c => c.String(nullable: false));
+        }
+    }
+}
