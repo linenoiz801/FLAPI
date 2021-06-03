1ce6f539483ec54341cd687ef3c7cd09ae2cf475
Adam Cohill <adamcohill16@gmail.com>
2021-06-03 19:57:23 -0400

    dunno

---
+++ FlAPI.Data/Migrations/202106012330140_Testing.cs
@@ -0,0 +1,36 @@
+﻿namespace FLAPI.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Testing : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Locations", "GameId", "dbo.Games");
+            DropForeignKey("dbo.Locations", "HistoryId", "dbo.Histories");
+            DropIndex("dbo.Locations", new[] { "GameId" });
+            DropIndex("dbo.Locations", new[] { "HistoryId" });
+            AlterColumn("dbo.Locations", "GameId", c => c.Int());
+            AlterColumn("dbo.Locations", "HistoryId", c => c.Int());
+            CreateIndex("dbo.Locations", "GameId");
+            CreateIndex("dbo.Locations", "HistoryId");
+            AddForeignKey("dbo.Locations", "GameId", "dbo.Games", "Id");
+            AddForeignKey("dbo.Locations", "HistoryId", "dbo.Histories", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Locations", "HistoryId", "dbo.Histories");
+            DropForeignKey("dbo.Locations", "GameId", "dbo.Games");
+            DropIndex("dbo.Locations", new[] { "HistoryId" });
+            DropIndex("dbo.Locations", new[] { "GameId" });
+            AlterColumn("dbo.Locations", "HistoryId", c => c.Int(nullable: false));
+            AlterColumn("dbo.Locations", "GameId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Locations", "HistoryId");
+            CreateIndex("dbo.Locations", "GameId");
+            AddForeignKey("dbo.Locations", "HistoryId", "dbo.Histories", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.Locations", "GameId", "dbo.Games", "Id", cascadeDelete: true);
+        }
+    }
+}
